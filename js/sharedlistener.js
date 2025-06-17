dff3ff75189e4a9830e8713cf31ef2968de3125f
abnerolivarez <165985162+abnerolivarez@users.noreply.github.com>
2025-06-17 17:54:47 +0800

    Update sharedlistener.js

--- js/sharedlistener.js
+++ js/sharedlistener.js
@@ -14,7 +14,7 @@ const globalSearchButton = document.getElementById("search-icon");
 performSearch = () =>{
     const searchTerm = globalSearchInput.value.trim();
     if(searchTerm){
-         window.location.href = `/product.html?search=${encodeURIComponent(searchTerm)}`;
+         window.location.href = `product.html?search=${encodeURIComponent(searchTerm)}`;
     }
 }
 
